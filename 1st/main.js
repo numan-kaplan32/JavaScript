136c54aa6e8b8757eeefa9e4ac2b5e4092975d58
Numan Kaplan <numankaplan32@gmail.com>
2025-07-02 20:16:00 +0300

    Add files via upload

---
+++ 1st/main.js
@@ -0,0 +1,17 @@
+// confirm('Welcome') 
+//True ve False şeklinde veri gönderir.
+
+// ------------------------------------------------------------------------------------------
+
+ 
+// console.log(typeof('Numan Kaplan'))//string
+
+// console.log(typeof(6))//number
+
+// console.log(typeof('6'))//string
+
+// console.log(typeof(2.5))//number
+
+// document.writeln('Numan Kaplan.') //sayfaya yazar
+
+// ------------------------------------------------------------------------------------------
